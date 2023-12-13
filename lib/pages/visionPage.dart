d25e1b02e24c6f96da744b3e190422d4f50a212e
KauaOrtiz <kauaortizz@gmail.com>
2023-12-13 01:14:08 -0300

    Feat: auto detect after choose a image

--- lib/pages/visionPage.dart
+++ lib/pages/visionPage.dart
@@ -4,14 +4,10 @@ import 'dart:ui';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'dart:async';
 import 'package:flutter_vision/flutter_vision.dart';
 import 'package:image_picker/image_picker.dart';
 
-enum Options { none, imagev8, frame}
-
-late List<CameraDescription> cameras;
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DartPluginRegistrant.ensureInitialized();
@@ -31,7 +27,6 @@ class VisionPage extends StatefulWidget {
 
 class _VisionPageState extends State<VisionPage> {
   late FlutterVision vision;
-  Options option = Options.none;
   @override
   void initState() {
     super.initState();
@@ -48,243 +43,8 @@ class _VisionPageState extends State<VisionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: task(option),
-      floatingActionButton: SpeedDial(
-        //margin bottom
-        icon: Icons.menu, //icon on Floating action button
-        activeIcon: Icons.close, //icon when menu is expanded on button
-        backgroundColor: Colors.black12, //background color of button
-        foregroundColor: Colors.white, //font color, icon color in button
-        activeBackgroundColor:
-        Colors.deepPurpleAccent, //background color when menu is expanded
-        activeForegroundColor: Colors.white,
-        visible: true,
-        closeManually: false,
-        curve: Curves.bounceIn,
-        overlayColor: Colors.black,
-        overlayOpacity: 0.5,
-        buttonSize: const Size(56.0, 56.0),
-        children: [
-          SpeedDialChild(
-            //speed dial child
-            child: const Icon(Icons.video_call),
-            backgroundColor: Colors.red,
-            foregroundColor: Colors.white,
-            label: 'Yolo on Frame',
-            labelStyle: const TextStyle(fontSize: 18.0),
-            onTap: () {
-              setState(() {
-                option = Options.frame;
-              });
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.camera),
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-            label: 'YoloV8 on Image',
-            labelStyle: const TextStyle(fontSize: 18.0),
-            onTap: () {
-              setState(() {
-                option = Options.imagev8;
-              });
-            },
-          ),
-        ],
-      ),
+      body: YoloImageV8(vision: vision),
     );
-  }
-
-  Widget task(Options option) {
-    if (option == Options.frame) {
-      return YoloVideo(vision: vision);
-    }
-    if (option == Options.imagev8) {
-      return YoloImageV8(vision: vision);
-    }
-    return const Center(child: Text("Choose Task"));
-  }
-}
-
-class YoloVideo extends StatefulWidget {
-  final FlutterVision vision;
-  const YoloVideo({Key? key, required this.vision}) : super(key: key);
-
-  @override
-  State<YoloVideo> createState() => _YoloVideoState();
-}
-
-class _YoloVideoState extends State<YoloVideo> {
-  late CameraController controller;
-  late List<Map<String, dynamic>> yoloResults;
-  CameraImage? cameraImage;
-  bool isLoaded = false;
-  bool isDetecting = false;
-
-  @override
-  void initState() {
-    super.initState();
-    init();
-  }
-
-  init() async {
-    cameras = await availableCameras();
-    controller = CameraController(cameras[0], ResolutionPreset.medium);
-    controller.initialize().then((value) {
-      loadYoloModel().then((value) {
-        setState(() {
-          isLoaded = true;
-          isDetecting = false;
-          yoloResults = [];
-        });
-      });
-    });
-  }
-
-  @override
-  void dispose() async {
-    super.dispose();
-    controller.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    if (!isLoaded) {
-      return const Scaffold(
-        body: Center(
-          child: Text("Model not loaded, waiting for it"),
-        ),
-      );
-    }
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Positioned(
-          left: 0,
-          top: 0,
-          width: controller.value.previewSize!.height,
-          height: controller.value.previewSize!.width,
-          child: CameraPreview(controller),
-        ),
-        ...displayBoxesAroundRecognizedObjects(size),
-        Positioned(
-          bottom: 75,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                  width: 5, color: Colors.white, style: BorderStyle.solid),
-            ),
-            child: isDetecting
-                ? IconButton(
-              onPressed: () async {
-                stopDetection();
-              },
-              icon: const Icon(
-                Icons.stop,
-                color: Colors.red,
-              ),
-              iconSize: 50,
-            )
-                : IconButton(
-              onPressed: () async {
-                await startDetection();
-              },
-              icon: const Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-              ),
-              iconSize: 50,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> loadYoloModel() async {
-    await widget.vision.loadYoloModel(
-        labels: 'assets/labels.txt',
-        modelPath: 'assets/yolov8n.tflite',
-        modelVersion: "yolov8",
-        numThreads: 1,
-        useGpu: true);
-    setState(() {
-      isLoaded = true;
-    });
-  }
-
-  Future<void> yoloOnFrame(CameraImage cameraImage) async {
-    final result = await widget.vision.yoloOnFrame(
-        bytesList: cameraImage.planes.map((plane) => plane.bytes).toList(),
-        imageHeight: cameraImage.height,
-        imageWidth: cameraImage.width,
-        iouThreshold: 0.4,
-        confThreshold: 0.4,
-        classThreshold: 0.5);
-    if (result.isNotEmpty) {
-      setState(() {
-        yoloResults = result;
-
-      });
-    }
-  }
-
-  Future<void> startDetection() async {
-    setState(() {
-      isDetecting = true;
-    });
-    if (controller.value.isStreamingImages) {
-      return;
-    }
-    await controller.startImageStream((image) async {
-      if (isDetecting) {
-        cameraImage = image;
-        yoloOnFrame(image);
-      }
-    });
-  }
-
-  Future<void> stopDetection() async {
-    setState(() {
-      isDetecting = false;
-      yoloResults.clear();
-    });
-  }
-
-  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
-    if (yoloResults.isEmpty) return [];
-    double factorX = screen.width / (cameraImage?.height ?? 1);
-    double factorY = screen.height / (cameraImage?.width ?? 1);
-
-    Color colorPick = const Color.fromARGB(255, 50, 233, 30);
-
-    return yoloResults.map((result) {
-      return Positioned(
-        left: result["box"][0] * factorX,
-        top: result["box"][1] * factorY,
-        width: (result["box"][2] - result["box"][0]) * factorX,
-        height: (result["box"][3] - result["box"][1]) * factorY,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-            border: Border.all(color: Colors.pink, width: 2.0),
-          ),
-          child: Text(
-            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(0)}%",
-            style: TextStyle(
-              background: Paint()..color = colorPick,
-              color: Colors.white,
-              fontSize: 18.0,
-            ),
-          ),
-        ),
-      );
-    }).toList();
   }
 }
 
@@ -342,10 +102,6 @@ class _YoloImageV8State extends State<YoloImageV8> {
                 onPressed: pickImage,
                 child: const Text("Pick an image"),
               ),
-              ElevatedButton(
-                onPressed: yoloOnImage,
-                child: const Text("Detect"),
-              )
             ],
           ),
         ),
@@ -375,6 +131,7 @@ class _YoloImageV8State extends State<YoloImageV8> {
       setState(() {
         imageFile = File(photo.path);
       });
+      yoloOnImage();
     }
   }
 
